875ab6f735eb820dca4ed56577782cb326098561
JeonHongShik <hongshik98@naver.com>
2023-10-18 15:54:18 +0900

    카카오로그인 환경설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk_common.dart';
 
 void main() {
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized;
+
+  KakaoSdk.init(
+    nativeAppKey: 'eabfebdd6a97136a8764ecfc05c8b8fe',
+    javaScriptAppKey: '1405eb959f73904bf61cb1f7163e37d4',
+  );
+
   runApp(const MyApp());
 }
 
